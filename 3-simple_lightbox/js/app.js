6243477a4d3d078ab1b4945f517285dc46211433
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-29 13:04:46 +0300

    capturing the click event on a link to an image
    using click(), attr() and event.preventDefault() methods

--- 3-simple_lightbox/js/app.js
+++ 3-simple_lightbox/js/app.js
@@ -2,11 +2,16 @@
 // Solution: create an overlay with the large image - lightbox
 
 // 1. Capture the click event on a link to an image
-   // 1.1. Show the overlay.
-   // 1.2. Update overlay with the image linked in the link
-   // 1.3. Get child's alt attribute and set caption
+$("#imageGallery a").click(function (event) {
+    event.preventDefault();
+    var href = $(this).attr("href");
+    console.log(href);
+    // 1.1. Show the overlay.
+    // 1.2. Update overlay with the image linked in the link
+    // 1.3. Get child's alt attribute and set caption
+});
 // 2. Add overlay
-   // 2.1. An image.
-   // 2.2. A caption
+    // 2.1. An image.
+    // 2.2. A caption
 // 3. When overlay is clicked
-   // 3.1. Hide the overlay
+    // 3.1. Hide the overlay
