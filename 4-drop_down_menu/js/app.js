7a21ddf2a4d18985a5b2aa5fa60d00d9b0cdf6c4
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-30 23:08:24 +0300

    step-by-step solution

--- 4-drop_down_menu/js/app.js
+++ 4-drop_down_menu/js/app.js
@@ -1,3 +1,15 @@
 // Problem: it looks gross in smaller browser widths and small devices
 // Solution: to hide the text links and swap them out with with a more appropriate navigation
 
+// Create a <select> and append to menu
+// Cycle over menu links
+    // create an option
+    // option's value is the href
+    // option's text is the text of link
+    // append option to select
+// Create button
+// Bind click to button
+    // go to select's location
+// Modify CSS to hide links on small width and show button and select
+    // also hides select and button on large width and show's links
+// Deal with selected options depending on current page
