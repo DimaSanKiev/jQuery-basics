cfe9b5bcdd84531fa1ed6501b261b1c9b597c961
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-30 23:55:10 +0300

    create a select and append to menu and cycling over menu links
    jQuery methods used: append(), each(), val(), text(), attr()

--- 4-drop_down_menu/js/app.js
+++ 4-drop_down_menu/js/app.js
@@ -1,12 +1,24 @@
 // Problem: it looks gross in smaller browser widths and small devices
 // Solution: to hide the text links and swap them out with with a more appropriate navigation
 
-// Create a <select> and append to menu
+// Create a <select> and append to #menu
+var $select = $("<select></select>");
+$("#menu").append($select);
+
 // Cycle over menu links
+$("#menu a").each(function () {
+    var $anchor = $(this);
     // create an option
+    var $option = $("<option></option>");
     // option's value is the href
+    $option.val($anchor.attr("href"));
     // option's text is the text of link
+    var anchorText = $anchor.text();
+    $option.text(anchorText);
     // append option to select
+    $select.append($option);
+});
+
 // Create button
 // Bind click to button
     // go to select's location
