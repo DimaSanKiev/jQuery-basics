0a91ec7f8ba3cda90c10d016f8931ea1031858db
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-08-01 01:28:18 +0300

    add steps to reach the solution

--- 6-drawing_app/js/app.js
+++ 6-drawing_app/js/app.js
@@ -1,2 +1,19 @@
 // Problem: no user interaction causes no change to application
 // Solution: when user interacts cause changes appropriately
+
+// When clicking on control list items
+    // deselect sibling elements
+    // select clicked element
+
+// When new color is pressed
+    // show color select or hide the color select
+
+// When color sliders change
+    // update the new color span
+
+// When add color is pressed
+    // append the color to the controls ul
+    // select the new color
+
+// On mouse events on the canvas
+    // draw lines
