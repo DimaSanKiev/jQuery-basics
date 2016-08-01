27e965876596fdb2ed0520786a7de6e05fa0b276
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-08-02 01:14:19 +0300

    append a new color on color palette when "add color" is pressed
    jQuery methods used: click(), css(), append(), on()

--- 6-drawing_app/js/app.js
+++ 6-drawing_app/js/app.js
@@ -4,7 +4,7 @@
 var color = $(".selected").css("background-color");
 
 // When clicking on control list items
-$(".controls li").click(function () {
+$(".controls").on("click", "li", function () {
     // deselect sibling elements
     $(this).siblings().removeClass("selected");
     // select clicked element
@@ -13,7 +13,7 @@ $(".controls li").click(function () {
     color = $(this).css("background-color");
 });
 
-// When new color is pressed
+// When "New color" is pressed
 $("#revealColorSelect").click(function () {
     // show color select or hide the color select
     $("#colorSelect").toggle();
@@ -24,16 +24,23 @@ function changeColor() {
     var r = $("#red").val();
     var g = $("#green").val();
     var b = $("#blue").val();
-    
+
     $("#newColor").css("background-color", "rgb(" + r + ", " + g + ", " + b + ")");
 }
 
 // When color sliders change
 $("input[type=range]").change(changeColor);
 
-// When add color is pressed
+// When "Add color" is pressed
+$("#addNewColor").click(function () {
     // append the color to the controls ul
+    var $newColor = $("<li></li>");
+    $newColor.css("background-color", $("#newColor").css("background-color"));
+    $(".controls ul").append($newColor);
     // select the new color
+    $newColor.click();
+
+});
 
 // On mouse events on the canvas
     // draw lines
